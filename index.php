ba4403d7b8aec2a8fd61042943aadd7cb7b3dc7c
1Rogue <spam.mehere@hotmail.com>
2013-09-19 13:39:01 -0400

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(-1);
+ini_set("display_errors", 1);
+
+function __autoload($class_name) {
+    include $class_name . '.php';
+}
+?>
+<html>
+    <head lang="en-US">
+        <title>Testing page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="assets/js/window.js"></script>
+        <script src="assets/js/jquery.js"></script>
+    </head>
+    <body>
+        Hello
+    </body>
+</html>
